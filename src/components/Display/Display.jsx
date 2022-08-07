efdb84110e58b52811112853eb81435a865e0bb9
AegisVP <vlad@pysarenko.com>
2022-08-07 02:35:09 +0200

    add dynamic BG color

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -5,6 +5,16 @@ import { Box } from 'components/Common/Box';
 import { lang } from '../App/App';
 
 const FeedbackStats = ({ state: { good, neutral, bad } }) => {
+  const totalVotes = Number(good + neutral + bad);
+  const percentPositive = Math.round((good / (good + neutral + bad)) * 100);
+  let rColor = (255 * (100 - (percentPositive - 50) * 2)) / 100;
+  let gColor = (255 * (percentPositive * 2)) / 100;
+
+  rColor = rColor > 255 ? 255 : rColor < 0 ? 0 : rColor;
+  gColor = gColor > 255 ? 255 : gColor < 0 ? 0 : gColor;
+
+  const bgColor = `rgba(${rColor}, ${gColor}, 0, 0.25)`;
+
   return (
     <Box borderTop="2px solid #DDDDDD">
       <Title>Statistics</Title>
@@ -18,10 +28,10 @@ const FeedbackStats = ({ state: { good, neutral, bad } }) => {
         {lang.bad.cc}: <Value>{bad}</Value>
       </StatRow>
       <StatRow backgroundColor="#f0f0f0">
-        Total: <Value>{good + neutral + bad}</Value>
+        Total: <Value>{totalVotes}</Value>
       </StatRow>
-      <StatRow backgroundColor="#f0fff0">
-        Positive %: <Value>{Math.round((good / (good + neutral + bad)) * 100)}%</Value>
+      <StatRow backgroundColor={bgColor}>
+        Positive %: <Value>{percentPositive}%</Value>
       </StatRow>
     </Box>
   );
